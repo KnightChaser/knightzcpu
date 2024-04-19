a42a84fa6cb3e8eb3f308cc5dc9cb050a71685d8
Lee Garam <agerio100@naver.com>
2024-04-19 23:16:01 +0900

    Use static_cast for type conversion

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@
 #include <cstdint>
 #include "cpu.hpp"
 
+typedef uint8_t cpuBitSize;
+
 int main() {
     // Create a new CPU instance
     // In this case, we are using a small and cute 8-bit CPU >w<
     // (Further, we can create 16-bit, 32-bit, or even 64-bit CPUs!)
-    CPU<uint8_t> cpu;
+    CPU<cpuBitSize> cpu;
 
     // Reset the CPU
     cpu.reset();
@@ -23,9 +25,10 @@ int main() {
     cpu.setRegisterValue("C", 30);
 
     // Get register values
-    std::cout << "Register A: " << (int)cpu.getRegisterValue("A") << std::endl;
-    std::cout << "Register B: " << (int)cpu.getRegisterValue("B") << std::endl;
-    std::cout << "Register C: " << (int)cpu.getRegisterValue("C") << std::endl;
+    // Note that std::cout treats uint8_t as a character type
+    std::cout << "Register A: " << static_cast<int>(cpu.getRegisterValue("A")) << std::endl;
+    std::cout << "Register B: " << static_cast<int>(cpu.getRegisterValue("B")) << std::endl;
+    std::cout << "Register C: " << static_cast<int>(cpu.getRegisterValue("C")) << std::endl;
 
     return 0;
 }
